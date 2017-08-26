a489b5aa687e5c0956bdb12b534f6ca9bf1742d8
debashisbswal <debashisbswal@gmail.com>
2017-08-26 17:58:27 +0530

    Add input fields for addUser mutation

--- app/graphql/mutations/user_mutations.rb
+++ app/graphql/mutations/user_mutations.rb
@@ -2,12 +2,13 @@ module UserMutations
   Create = GraphQL::Relay::Mutation.define do
     name "AddUser"
 
+    input_field :name, !types.String
     input_field :email, !types.String
     return_field :user, UserType
 
     resolve ->(object, inputs, ctx) {
 
-      user = User.create(email: inputs[:email])
+      user = User.create(name: inputs[:name], email: inputs[:email])
 
       if user.save
         { user: user }
