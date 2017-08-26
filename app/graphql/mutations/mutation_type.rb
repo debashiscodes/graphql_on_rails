7306d0af0fb27d4683f67f8aa99cf4d5d1c07250
debashisbswal <debashisbswal@gmail.com>
2017-08-26 18:52:34 +0530

    Add comments to MutationType

--- app/graphql/mutations/mutation_type.rb
+++ app/graphql/mutations/mutation_type.rb
@@ -3,10 +3,12 @@ MutationType = GraphQL::ObjectType.define do
   description "The mutation root"
 
 
+  #### For User ####
   field :addUser, field: UserMutations::Create.field
   field :updateUser, field: UserMutations::Update.field
   field :destroyUser, field: UserMutations::Destroy.field
 
+  #### For Post ####
   field :addPost, field: PostMutations::Create.field
 
 end
